dbec3688237626ae9ea51b990dc4a6835a476899
Serhii Sosnytskyi <serhii.sosnytskyi@chisw.com>
2025-12-31 23:35:19 +0200

    feat: delete method is ready! HAPPY NEW YEAR!

--- backend/src/task/task.controller.ts
+++ backend/src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -30,5 +30,10 @@ export class TaskController {
   @Patch(':id')
   patchUpdate(@Param('id') id: string, @Body() dto: Partial<UpdateTaskDto>) {
     return this.taskService.patchUpdate(+id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskService.delete(+id);
   }
 }
