4dc00792ebffc32b13e4a7f237030192d747a49a
Serhii Sosnytskyi <serhii.sosnytskyi@chisw.com>
2026-01-06 13:46:14 +0200

    feat: played with nest decorators for controllers

--- backend/src/movie/movie.controller.ts
+++ backend/src/movie/movie.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import type { Request, Response } from 'express';
 
 @Controller('movie')
 export class MovieController {
@@ -16,8 +27,44 @@ export class MovieController {
     return this.movieService.getMoviesByGenre(genre);
   }
 
+  @Get('genres')
+  getGenres() {
+    return this.movieService.getGenres();
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    return headers;
+  }
+
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: string) {
+    return { userAgent };
+  }
+
+  @Get('request')
+  getRequestDetails(@Req() req: Request) {
+    return {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      query: req.query,
+      params: req.params,
+    };
+  }
+
+  @Get('response')
+  getResponseDetails(@Res() res: Response) {
+    res.status(201).json({ message: 'This is a custom response' });
+  }
+
+  @Get(':id')
+  getMyId(@Param('id') id: string) {
+    return { id };
+  }
+
   @Post('add')
-  addMovieToGenre(@Body() dto: CreateMovieDto){
+  addMovieToGenre(@Body() dto: CreateMovieDto) {
     return this.movieService.addMovieToGenre(dto);
   }
 }
